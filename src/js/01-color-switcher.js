a1bd7fb349d87bcd5f8b1daa5c7cb054f3218035
EduardOshurkov <drd2993@gmail.com>
2022-06-05 19:41:55 +0300

    first commit task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
 
+const refs = {
+    body: document.querySelector('body'),
+    startBtn: document.querySelector('button[data-start]'),
+    stopBtn: document.querySelector('button[data-stop]'),
+};
+
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
+
+let changes = null;
+
+refs.startBtn.addEventListener('click', () => {
+    const changes = setInterval(() => {
+        refs.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+    refs.stopBtn.addEventListener('click', () => {
+    clearInterval(changes);
+});
+});
